d1688a46f354c03efc0397736fee75d7e7f0ccac
Cody Mikol <codymikol@gmail.com>
2022-02-13 19:55:39 -0500

    fix(main): allow intellij to pick up main module
    I can't use the debugger in intellij unless I add these
    suppresed args here for some reason...
    
    Fixes N/A

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.ui.window.application
 
-fun main() = application { App(this) }
+@Suppress("UNUSED_PARAMETER")
+fun main(args: Array<String>) = application { App(this) }
