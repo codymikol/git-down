52fa62df43d9d4ca70c7b6f10832ce69e7d92c3a
Cody Mikol <codymikol@gmail.com>
2022-06-10 22:45:47 -0400

    feat(deps): bump dependencies
    ...
    
    Fixes N/A

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
     kotlin("jvm") version "1.6.10"
@@ -9,9 +8,9 @@ plugins {
     id("com.google.devtools.ksp") version "1.6.10-1.0.2"
 }
 
-val koin_version = "3.2.0"
-val koin_ksp_version= "1.0.0-beta-2"
-val ksp_version = "1.6.10-1.0.2"
+val koinVersion = "3.2.0"
+val koinKspVersion= "1.0.0-beta-2"
+val kspVersion = "1.6.10-1.0.2"
 
 group = "com.codymikol"
 version = "1.0"
@@ -29,27 +28,27 @@ repositories {
 }
 
 dependencies {
-    api("io.insert-koin:koin-core:$koin_version")
-    api("io.insert-koin:koin-annotations:$koin_ksp_version")
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
-    ksp("io.insert-koin:koin-ksp-compiler:$koin_ksp_version")
-    implementation("io.insert-koin:koin-core:$koin_version")
+    api("io.insert-koin:koin-core:$koinVersion")
+    api("io.insert-koin:koin-annotations:$koinKspVersion")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    ksp("io.insert-koin:koin-ksp-compiler:$koinKspVersion")
+    implementation("io.insert-koin:koin-core:$koinVersion")
     implementation(compose.desktop.currentOs)
     implementation("net.harawata:appdirs:1.2.1")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
     implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
-    testImplementation("io.insert-koin:koin-test:$koin_version")
-    testImplementation("io.kotest:kotest-runner-junit5:5.0.1")
-    testImplementation("io.kotest:kotest-assertions-core:5.0.1")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.1.0.202203080745-r")
+    testImplementation("io.insert-koin:koin-test:$koinVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.3.0")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
@@ -58,7 +57,7 @@ compose.desktop {
         mainClass = "MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "reboot-desktop"
+            packageName = "gitdown"
             packageVersion = "1.0.0"
         }
     }
