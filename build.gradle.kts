82e70f25f0f43f284da6019021bba513fb6f72b8
Cody Mikol <codymikol@gmail.com>
2022-10-27 18:33:28 -0400

    chore(deps): bump dependencies
    ...
    
    Fixes N/A

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -8,8 +7,8 @@ plugins {
     id("com.google.devtools.ksp") version "1.7.20-1.0.6"
 }
 
-val koinVersion = "3.2.0"
-val koinKspVersion= "1.0.0"
+val koinVersion = "3.2.2"
+val koinKspVersion= "1.0.3"
 val kspVersion = "1.6.10-1.0.2"
 
 group = "com.codymikol"
@@ -30,18 +29,18 @@ repositories {
 dependencies {
     api("io.insert-koin:koin-core:$koinVersion")
     api("io.insert-koin:koin-annotations:$koinKspVersion")
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
     ksp("io.insert-koin:koin-ksp-compiler:$koinKspVersion")
     implementation("io.insert-koin:koin-core:$koinVersion")
     implementation(compose.desktop.currentOs)
     implementation("net.harawata:appdirs:1.2.1")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.4")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.4")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0-rc1")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
     testImplementation("io.insert-koin:koin-test:$koinVersion")
-    testImplementation("io.kotest:kotest-runner-junit5:5.3.1")
-    testImplementation("io.kotest:kotest-assertions-core:5.3.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.2")
 }
 
 tasks.withType<Test> {
