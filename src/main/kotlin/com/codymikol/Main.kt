codymikol/git-down

commit c03cc53d43cf1d90365e0b692c4d80b6e6bc9c28

chore: push changes in my stage that have been sitting forever
Ideally this would all be organized, but I haven't comitted
in such a long time and I don't have the full context for all
of these changes. Sorry!

Fixes N/A

src/main/kotlin/com/codymikol/Main.kt
@@ -2,27 +2,22 @@ package com.codymikol// Copyright 2000-2021 JetBrains s.r.o. and contributors. U
 import androidx.compose.ui.window.application
 import com.codymikol.config.BeansModule
 import com.codymikol.config.RepositoriesModule
-import com.codymikol.state.GitDownState
 import org.koin.core.context.startKoin
 import org.koin.ksp.generated.module
 
-class Main {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) = application {
+fun main(args: Array<String>) = application {
 
-            // todo(mikol): we should move this into a load directory service and make it more robust eventually...
-            if(args.size == 1) {
-                println("requested directory: " + args[0])
-                GitDownState.gitDirectory.value = args[0] + "/.git"
-            }
-
-            startKoin { modules(
-                RepositoriesModule().module,
-                BeansModule().module,
-            ) }
-            App(this)
-        }
+    // todo(mikol): we should move this into a load directory service and make it more robust eventually...
+    if (args.size == 0) {
+//        println("requested directory: " + args[-1])
+//        GitDownState.gitDirectory.value = args[-1] + "/.git"
     }
-}
 
+    startKoin {
+        modules(
+            RepositoriesModule().module,
+            BeansModule().module,
+        )
+    }
+    App(this)
+}

build.gradle.kts
@@ -56,7 +56,7 @@ tasks.withType<Test>().configureEach {
   }
 }
 
-tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "17" }
+tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "21" }
 
 group = "com.codymikol"
 version = "1.0"

flake.nix
@@ -8,49 +8,38 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
 
-      packages.default = pkgs.stdenv.mkDerivation( finalAttrs: {
-        
-        pname = "git-down";
-        version = "0.1.0";
-        src = ./.;
-
-        nativeBuildInputs = [ pkgs.gradle ];
-
-        mtimCache = pkgs.gradle.fetchDeps {
-          inherit (finalAttrs) pname;
-          data = ./deps.json;
-        };
-
-        # this is required for using mitm-cache on Darwin
-        __darwinAllowLocalNetworking = true;
-
-        buildInputs = [ pkgs.jdk11 pkgs.mesa ];
-
-      }); 
-
       devShells.default = pkgs.mkShell {
+
+          _JAVA_OPTIONS = "-Dswing.useSystemFileChooser=true";
         
-        packages = [
+          packages = [
             pkgs.git
-            pkgs.temurin-bin-17
+            pkgs.temurin-bin-21
             pkgs.mesa
             pkgs.libGL
             pkgs.libGLU
-            pkgs.xorg.libX11
             pkgs.xorg.libXext
             pkgs.fontconfig
-        ];
+            pkgs.xorg.libX11
+            pkgs.xorg.libXrandr
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXi
+            pkgs.xorg.libXext
+            pkgs.fontconfig
+          ];
 
-        shellHook = ''
-          echo "Setting up OpenGL environment...";
-          LD_LIBRARY_PATH="${pkgs.libGL}/lib/:${pkgs.stdenv.cc.cc.lib}/lib/";
-        '';
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.mesa
+            pkgs.libglvnd
+            pkgs.xorg.libX11
+            pkgs.xorg.libXrandr
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXi
+            pkgs.xorg.libXext
+            pkgs.fontconfig
+          ];
 
       };
 
-      meta.sourceProvenance = with pkgs.lib.sourceTypes; [
-        fromSource
-        binaryBytecode # mitm cache
-      ];
     });
 }

src/main/kotlin/com/codymikol/components/SlimButton.kt
@@ -34,12 +34,13 @@ fun SlimButton(
     text: String,
     disabled: Boolean = false,
     onClick: () -> Unit = {},
+    modifier: Modifier = Modifier,
 ) =
     Button(
         onClick = onClick,
         enabled = !disabled,
         colors = SlimButtonColors(),
-        modifier = Modifier.height(28.dp)
+        modifier = Modifier.height(28.dp).then(modifier)
     ) { Text(
         text,
         fontSize = 10.sp,
@@ -52,4 +53,4 @@ fun SlimButton(
 @Preview
 fun PreviewSlimButton() = /** noop **/
     /** noop **/
-    SlimButton("Hello") { /** noop **/ }
+    SlimButton("Hello")

src/main/kotlin/com/codymikol/components/commit/BottomStatusMessage.kt
@@ -1,41 +1,1 @@
 package com.codymikol.components.commit
-
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import com.codymikol.data.Colors
-import com.codymikol.state.GitDownState
-
-@Composable
-private fun RegularText(_text: String) = Text(_text, color = Colors.LightGrayText, fontSize = 12.sp)
-
-@Composable
-private fun BoldText(_text: String) =
-    Text(_text, fontWeight = FontWeight.Bold, color = Colors.LightGrayText, fontSize = 12.sp)
-
-private fun getObjectName(): String = when (GitDownState.isDetached.value) {
-    true -> "HEAD"
-    false -> GitDownState.branchName.value
-}
-
-private fun getObjectNamePrefix(): String = "on " + when (GitDownState.isDetached.value) {
-    true -> "detached"
-    false -> "branch"
-} + " "
-
-@Composable
-fun BottomStatusMessage() = Row {
-    RegularText("Commiting as ")
-    BoldText("${GitDownState.committingAsName.value} <${GitDownState.comittingAsEmail.value}> ")
-    RegularText(getObjectNamePrefix())
-    BoldText(getObjectName())
-}
-
-@Composable
-@Preview
-fun TestStatusMessage() {
-    return BottomStatusMessage()
-}

src/main/kotlin/com/codymikol/components/commit/CommitBottomToolbar.kt
@@ -9,6 +9,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.codymikol.components.SlimButton
@@ -16,12 +18,48 @@ import com.codymikol.data.Colors
 import com.codymikol.extensions.amendAll
 import com.codymikol.extensions.commitAll
 import com.codymikol.extensions.unstageAll
-import kotlinx.coroutines.launch
 import com.codymikol.state.GitDownState
+import kotlinx.coroutines.launch
+
+@Composable
+private fun RegularText(_text: String, modifier: Modifier = Modifier) = Text(
+    _text,
+    color = Colors.LightGrayText,
+    fontSize = 12.sp,
+    modifier = modifier,
+)
+
+@Composable
+fun BoldText(_text: String, modifier: Modifier = Modifier) = Text(
+    _text,
+    fontWeight = FontWeight.Bold,
+    color = Colors.LightGrayText,
+    fontSize = 12.sp,
+    overflow = TextOverflow.Ellipsis,
+    modifier = modifier,
+)
+
+
+fun getObjectName(isDetatchedHead: Boolean, branchName: String): String = when (isDetatchedHead) {
+    true -> "HEAD"
+    false -> branchName
+}
+
+fun getObjectNamePrefix(isDetatchedHead: Boolean): String = "on " + when (isDetatchedHead) {
+    true -> "detached"
+    false -> "branch"
+} + " "
 
 @Composable
 @Preview
-fun CommitBottomToolbar(commitMessage: MutableState<String>) {
+fun CommitBottomToolbar(
+    commitMessage: MutableState<String>,
+    committingAsName: String = GitDownState.committingAsName.value,
+    committingAsEmail: String = GitDownState.comittingAsEmail.value,
+    isDetachedHead: Boolean = GitDownState.isDetached.value,
+    branchName: String = GitDownState.branchName.value,
+    indexIsEmpty: Boolean = GitDownState.indexIsEmpty.value,
+) {
 
     val amendEnabled = remember { mutableStateOf(false) }
 
@@ -37,41 +75,87 @@ fun CommitBottomToolbar(commitMessage: MutableState<String>) {
         }
     }
 
+    BoxWithConstraints() {
+
+    val constraints = this
+
+    val unstageWidth = 100.dp
+    val commitWidth = 100.dp
+    val amendCheckboxWidth = 50.dp
+    val amendTextWidth = 80.dp
+    val committingAsWidth = 90.dp
+
+        val totalFixedWidth = unstageWidth + commitWidth + amendTextWidth + amendCheckboxWidth + committingAsWidth
+
+        val showComittingAs = constraints.maxWidth > totalFixedWidth
+
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier.background(color = Colors.LightGrayBackground).fillMaxWidth().requiredHeight(48.dp)
             .padding(10.dp)
     ) {
-        SlimButton("Unstage All") {
+        SlimButton("Unstage All", onClick = {
             scope.launch {
                 GitDownState.git.value.unstageAll()
                 GitDownState.selectedFiles.clear()
             }
-        }
-        BottomStatusMessage()
+        })
+
+
+        if(showComittingAs) RegularText("Commiting as ", modifier = Modifier.requiredWidth(committingAsWidth))
+        BoldText("$committingAsName <$committingAsEmail> ")
+        RegularText(getObjectNamePrefix(isDetachedHead))
+        BoldText(getObjectName(isDetachedHead, branchName), modifier = Modifier.widthIn(max = 100.dp, min = 100.dp))
+
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(modifier = Modifier, checked = amendEnabled.value, onCheckedChange = { toggleAmendHead() })
-            Text(modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp), text = "Amend Head", fontSize = 12.sp, color = Color.White)
-            SlimButton("Commit", disabled = GitDownState.indexIsEmpty.value && !amendEnabled.value) {
-                scope.launch {
+            Checkbox(
+                modifier = Modifier.requiredWidth(50.dp),
+                checked = amendEnabled.value,
+                onCheckedChange = { toggleAmendHead() })
+            Text(
+                modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp).requiredWidth(80.dp),
+                text = "Amend Head",
+                fontSize = 12.sp,
+                color = Color.White,
+                overflow = TextOverflow.Ellipsis,
+            )
+            SlimButton(
+                "Commit",
+                modifier = Modifier.requiredWidth(100.dp),
+                disabled = indexIsEmpty && !amendEnabled.value,
+                onClick = {
+                    scope.launch {
 
-                    when(amendEnabled.value) {
-                        true -> {
-                            GitDownState.git.value.amendAll(commitMessage.value)
-                            amendEnabled.value = false
+                        when (amendEnabled.value) {
+                            true -> {
+                                GitDownState.git.value.amendAll(commitMessage.value)
+                                amendEnabled.value = false
+                            }
+
+                            false -> {
+                                GitDownState.git.value.commitAll(commitMessage.value)
+                            }
                         }
-                        false -> {
-                            GitDownState.git.value.commitAll(commitMessage.value)
-                        }
+
+                        GitDownState.selectedFiles.clear()
+                        commitMessage.value = ""
+
                     }
-
-                    GitDownState.selectedFiles.clear()
-                    commitMessage.value = ""
-
-                }
-            }
+                })
         }
+    }
     }
 
 }
+
+@Composable
+@Preview
+fun previewCommitBottomToolbar() = CommitBottomToolbar(
+    commitMessage = mutableStateOf(""),
+    committingAsName = "Cody Mikol",
+    committingAsEmail = "codymikol@gmail.com",
+    isDetachedHead = false,
+    branchName = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    indexIsEmpty = false,
+)

src/main/kotlin/com/codymikol/components/commit/diff/file/header/action/buttons/StageLinesButton.kt
@@ -2,10 +2,10 @@ package com.codymikol.components.commit.diff.file.header.action.buttons
 
 import androidx.compose.runtime.Composable
 import com.codymikol.data.diff.FileDeltaNode
-import com.codymikol.extensions.stageLines
+import com.codymikol.extensions.stageLinesForAddedFile
 import com.codymikol.state.GitDownState
 
 @Composable
 fun StageLinesButton(fileDeltaNode: FileDeltaNode) = FileHeaderButton("Stage Lines") {
-    GitDownState.git.value.stageLines(fileDeltaNode.getSelectedLines())
+    GitDownState.git.value.stageLinesForAddedFile(fileDeltaNode.getSelectedLines())
 }

commit 38300429692b638b08f58b3fdb4344fc78786553

fix(cli): restore directory opening via command line args
The args handling in Main.kt had been silently broken since
c03cc53: the condition was inverted to args.size == 0, the body
was commented out, and it referenced an unimported GitDownState.
Route the single path argument through handleDirectorySelection
so it behaves the same as picking a directory from the UI.

docs(cli): add CLI documentation guide

Closes #112

src/main/kotlin/com/codymikol/Main.kt
@@ -3,16 +3,11 @@ import androidx.compose.ui.window.application
 import com.codymikol.config.BeansModule
 import com.codymikol.config.RepositoriesModule
 import com.codymikol.config.ServicesModule
+import com.codymikol.windows.handleDirectorySelection
 import org.koin.core.context.startKoin
 import org.koin.ksp.generated.module
 
 fun main(args: Array<String>) = application {
-
-    // todo(mikol): we should move this into a load directory service and make it more robust eventually...
-    if (args.size == 0) {
-//        println("requested directory: " + args[-1])
-//        GitDownState.gitDirectory.value = args[-1] + "/.git"
-    }
 
     startKoin {
         modules(
@@ -21,5 +16,11 @@ fun main(args: Array<String>) = application {
             ServicesModule().module,
         )
     }
+
+    // todo(mikol): we should move this into a load directory service and make it more robust eventually...
+    if (args.size == 1) {
+        handleDirectorySelection(args[0] + "/.git")
+    }
+
     App(this)
 }